9e241623aae31a9e6429b2efc2d62631149b347a
Kristiansen, Joakim <Joakim.Kristiansen@ssb.no>
2017-01-12 20:43:23 +0100

    Added - Binding between redux state and localstorage

--- src/store/index.js
+++ src/store/index.js
@@ -3,9 +3,12 @@ import ReduxPromise from 'redux-promise';
 import reducers from '../reducers';
 import DevTools from '../containers/devTools';
 
+const LOCAL_STORAGE_KEY = 'SuperAwesomeAlbumSearch';
+
 // May be used to store default values at reducers key's
-const initialState = {
-};
+// Check if something is saved to localstorage, if so use it as initial state.
+const stateFromLocalStorage = localStorage.getItem(LOCAL_STORAGE_KEY);
+const initialState = stateFromLocalStorage ? JSON.parse(stateFromLocalStorage) : {};
 
 // Include to use extra features as middleware and dev tools
 const enhancers = compose(
@@ -13,5 +16,11 @@ const enhancers = compose(
     DevTools.instrument()
 );
 
+
 // The 'store' is the container for redux data
 export const store = createStore(reducers, initialState, enhancers);
+
+// Bind changes to state to be saved to localstorage
+store.subscribe(()=>{
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(store.getState()))
+});
