d8b02ecae2100ebbf092ea1290b05d093d24c60e
Nina Smørsgård <nina.m.smorsgard@gmail.com>
2017-01-12 13:30:24 +0100

    removed initial state data

--- src/store/index.js
+++ src/store/index.js
@@ -5,24 +5,7 @@ import DevTools from '../containers/devTools';
 
 // May be used to store default values at reducers key's
 const initialState = {
-/*    albums: [
-        {
-            id: '3vGtqTr5he9uQfusQWJ0oC',
-            url: "https://i.scdn.co/image/463e545ef86af314c4b7c79ff17892531a80770e",
-            name: "Platinum & Gold Collection"
-        },
-        {
-            id: '7IW3NEq3Fxtm7FhOcosnBy',
-            url: 'https://i.scdn.co/image/8e8afc9db649eaf32a33e38a501a8bd7f27a9591',
-            name: '50'
-        },
-        {
-            id:'4HuGWbYCiCvWvPo7KLWUnj',
-            url: 'https://i.scdn.co/image/05d4e295b9149b2f74762bba540714dd79d2fae5',
-            name: 'Never Gonna Give You Up'
-        }
-    ]
-*/};
+};
 
 // Include to use extra features as middleware and dev tools
 const enhancers = compose(
