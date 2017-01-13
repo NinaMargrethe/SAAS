0e2d3640672507245a346ce92331e0cfe5fafec6
Kristiansen, Joakim <Joakim.Kristiansen@ssb.no>
2017-01-13 09:24:59 +0100

    Updated - switched localstorage for redux-persist enhancer

--- src/store/index.js
+++ src/store/index.js
@@ -2,25 +2,20 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import ReduxPromise from 'redux-promise';
 import reducers from '../reducers';
 import DevTools from '../containers/devTools';
-
-const LOCAL_STORAGE_KEY = '1SuperAwesomeAlbumSearch';
-
-// May be used to store default values at reducers key's
-// Check if something is saved to localstorage, if so use it as initial state.
-const stateFromLocalStorage = localStorage.getItem(LOCAL_STORAGE_KEY);
-const initialState = stateFromLocalStorage ? JSON.parse(stateFromLocalStorage) : {};
+import { persistStore, autoRehydrate } from 'redux-persist';
 
 // Include to use extra features as middleware and dev tools
 const enhancers = compose(
     applyMiddleware(ReduxPromise),
+    autoRehydrate(), // For localstorage
     DevTools.instrument()
 );
 
 
 // The 'store' is the container for redux data
-export const store = createStore(reducers, initialState, enhancers);
+const store = createStore(reducers, undefined, enhancers);
 
-// Bind changes to state to be saved to localstorage
-store.subscribe(()=>{
-    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(store.getState()))
-});
+// Store state - except for the search reducer
+persistStore(store, { blacklist: ['search'] });
+
+export { store };
